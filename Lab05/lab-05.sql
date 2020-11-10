899b818ae503acc2baacc767c96622f609c4c911
FunnyFoxxy <Fartunoman@gmail.com>
2020-11-10 16:16:39 +0300

    Files

---
+++ Lab05/lab-05.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS list;
+CREATE TABLE list (name TEXT, value INT, image TEXT);
+
+INSERT INTO list VALUE ('Aplle' 	,10	, 'img-apple.png');
+INSERT INTO list VALUE ('Blueberry' 	,100	, 'img-blueberry.png');
+INSERT INTO list VALUE ('Cherry' 	,30	, 'img-cherry.png');
+INSERT INTO list VALUE ('Grapes' 	,70	, 'img-grapes.png');😘️🤣️
+INSERT INTO list VALUE ('Lamon' 	,3	, 'img-lamon.png');
+INSERT INTO list VALUE ('Orange' 	,5	, 'img-orange.png');
+INSERT INTO list VALUE ('Pear' 	,7	, 'img-pear.png');
+INSERT INTO list VALUE ('Paspberry' 	,25	, 'img-paspberry.png');
+INSERT INTO list VALUE ('Strawberry' 	,40	, 'img-trawberry.png');
+
