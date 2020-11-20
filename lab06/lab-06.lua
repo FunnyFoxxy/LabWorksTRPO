0332f0c53045a89a45897c2e7291877227e1ea37
FunnyFoxxy <Fartunoman@gmial.com>
2020-11-20 09:00:24 +0300

    lab-06

--- lab06/lab-06.lua
+++ lab06/lab-06.lua
@@ -1,8 +1,9 @@
+
 lgi = require 'lgi'
 sqlite = require 'lsqlite3'
 
 gtk = lgi.Gtk
-pixbuf = lgi.GbkPixbuf.Pixbuf
+pixbuf = lgi.GdkPixbuf.Pixbuf
 
 gtk.init()
 
@@ -11,22 +12,23 @@ bld: add_from_file('lab-06.glade')
 
 ui = bld.objects
 
-rdr_txt = gtk.CellRendererText{}
-rdr_pix = gtk.CellRendererPixbuf{}
+rdr_txt = gtk.CellRendererText {}
+rdr_pix = gtk.CellRendererPixbuf {}
 
-c1 = gtk.TreeViewColomn{ title = 'Name', {rdr_txt, {text = 1}}	}
-c1 = gtk.TreeViewColomn{ title = 'Value', {rdr_txt, {text = 2}}	}
-c1 = gtk.TreeViewColomn{ title = 'Image', {rdr_txt, {pixbuf = 3}}	}
+c1 = gtk.TreeViewColumn{ title = 'Name',	{rdr_txt, 	{text = 1}}		}
+c2 = gtk.TreeViewColumn{ title = 'Value', {rdr_txt, {text = 2}}		}
+c3 = gtk.TreeViewColumn{ title = 'Image', {rdr_pix, {pixbuf = 3}}	}
 
-ui.list_items:append_column(c1)
-ui.list_items:append_colomn(c2)
-ui.list_items:append_colomn(c3)
+ui.lst_items:append_column(c1)
+ui.lst_items:append_colomn(c2)
+ui.lst_items:append_colomn(c3)
 
 db = sqlite.open('lab-06.db')
+
 for row in db:nrows('SELECT * FROM list') do
 px = pixbuf.new_from_file(row.image)
 
-el = ui.stor_items;append()
+el = ui.stor_items:append()
 ui.stor_items[el] = {[i] = row.name, [2] = row.value, [3] = px}
 end
 
--- lab06/lab-06.sh
+++ lab06/lab-06.sh
@@ -1,13 +1,2 @@
-DROP TABLE IF EXITS LIST;
-CREATE TABLE list {name TEXT, value INT, image TEXT);
-
-INSERT INTO VALUE ('Apple'	, 10	,'img-apple.png'	);
-INSERT INTO VALUE ('BlueBerry'	, 100	,'ing-blueberry.png'	);
-INSERT INTO VALUE ('Cherry'	, 30	,'ing-cherry.png'	);
-INSERT INTO VALUE ('Grapes'	, 70	,'ing-grapes.png'	);
-INSERT INTO VALUE ('Lemon'	, 3	,'ing-lemon.png'	);
-INSERT INTO VALUE ('Orange'	, 5	,'ing-orange.png'	);
-INSERT INTO VALUE ('Pear'	, 7	,'ing-pear.png'		);
-INSERT INTO VALUE ('Raspberry'	, 25	,'ing-raspberry.png'	);
-INSERT INTO VALUE ('Strawderry'	, 40	,'ing-strawderry.png'	);
-
+rm -f lab-06.db
+cat lab-06.sql | sqlite3 lab-06.db
